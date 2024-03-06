51e19862798d66e7cbea6577de6af6259d7213dd
ques-vehikl <61066147+ques-vehikl@users.noreply.github.com>
2024-03-06 17:05:50 -0500

    [WIP] player list

--- src/components/TrackGame.tsx
+++ src/components/TrackGame.tsx
@@ -1,9 +1,35 @@
+import React, {useState} from "react";
+import {IonButton, IonInput, IonItem, IonLabel, IonList} from "@ionic/react";
 
 
 const TrackGame: React.FC = () => {
+    const [playerName, setPlayerName] = useState('');
+    const [playerList, setPlayerList] = useState<Array<string>>([""]);
+    
   return (
     <div className="container">
       <strong>Track Game</strong>
+        <IonInput 
+            data-testid="player-name"
+            label="Player name" 
+            placeholder="Enter player name" 
+            onIonChange={(e) => setPlayerName(e.detail.value ?? '')} 
+            value={playerName}>
+        </IonInput>
+        <IonButton onClick={() => {
+            setPlayerList([...playerList, playerName])
+            setPlayerName('');
+            
+        }}>submit</IonButton>
+        <IonList>
+            {playerList.map((player, index) => {
+                return (
+                    <IonItem key={index}>
+                        <IonLabel>{player}</IonLabel>
+                    </IonItem>
+                )
+            })}
+        </IonList>
     </div>
   );
 };
