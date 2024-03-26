b1cc6ca6d9444d36fee4d4a1a50049023f6131cb
felloBonello <justin.w.bonello@gmail.com>
2024-03-26 16:47:49 -0400

    order group

--- src/components/TrackGame.test.tsx
+++ src/components/TrackGame.test.tsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import {render, screen, fireEvent, waitFor, within} from '@testing-library/react';
+import React from "react";
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  within,
+} from "@testing-library/react";
 import TrackGame from "./TrackGame";
-import {IonApp} from "@ionic/react";
-import {getIonChangeEvent} from "../helpers/test/events";
+import { IonApp } from "@ionic/react";
+import { getIonChangeEvent } from "../helpers/test/events";
 
-describe('Track Game', () => {
-  it('renders', () => {
+describe("Track Game", () => {
+  it("renders", () => {
     render(<TrackGame />);
-    screen.getByText('Track Game')
+    screen.getByText("Track Game");
   });
-  
-  it('adds a new player to the list', async () => {
-    render(<IonApp><TrackGame /></IonApp>);
 
-    const playerName = 'John Doe'
+  it("adds a new player to the list", async () => {
+    render(
+      <IonApp>
+        <TrackGame />
+      </IonApp>,
+    );
 
-    const input = screen.getByTestId('player-name')
+    const playerName = "John Doe";
+
+    const input = screen.getByTestId("player-name");
     fireEvent(input, getIonChangeEvent(playerName));
 
-    expect(input).toHaveValue(playerName)
+    expect(input).toHaveValue(playerName);
 
-    const submitButton = screen.getByTestId('submit-button')
-    fireEvent.click(submitButton)
+    const submitButton = screen.getByTestId("submit-button");
+    fireEvent.click(submitButton);
 
-    const playerList = screen.getByTestId('player-list')
-    within(playerList).getByText(playerName)
+    const playerList = screen.getByTestId("player-list");
+    within(playerList).getByText(playerName);
   });
-})
-
-
+});
--- src/components/TrackGame.tsx
+++ src/components/TrackGame.tsx
@@ -1,39 +1,60 @@
-import React, {useState} from "react";
-import {IonButton, IonInput, IonItem, IonLabel, IonList} from "@ionic/react";
-
+import React, { useState } from "react";
+import {
+  IonButton,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonReorder,
+  IonReorderGroup,
+  ItemReorderEventDetail,
+} from "@ionic/react";
 
 const TrackGame: React.FC = () => {
-    const [playerName, setPlayerName] = useState('');
-    const [playerList, setPlayerList] = useState<Array<string>>([""]);
-    
+  const [playerName, setPlayerName] = useState("");
+  const [playerList, setPlayerList] = useState<Array<string>>([]);
+
+  function handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
+    setPlayerList(event.detail.complete(playerList));
+  }
+
   return (
     <div className="container">
       <strong>Track Game</strong>
-        <IonInput 
-            data-testid="player-name"
-            label="Player name" 
-            placeholder="Enter player name" 
-            onIonChange={(e) =>
-              setPlayerName(e.detail.value ?? '')}
-            value={playerName}>
-        </IonInput>
-        <IonButton
-          data-testid="submit-button"
-          onClick={() => {
-            setPlayerList([...playerList, playerName])
-            setPlayerName('');
-            
-        }}>submit</IonButton>
-        <IonList
-          data-testid="player-list">
-            {playerList.map((player, index) => {
-                return (
-                    <IonItem key={index}>
-                        <IonLabel>{player}</IonLabel>
-                    </IonItem>
-                )
-            })}
-        </IonList>
+      <IonInput
+        data-testid="player-name"
+        label="Player name"
+        placeholder="Enter player name"
+        onIonChange={(e) => setPlayerName(e.detail.value ?? "")}
+        value={playerName}
+      ></IonInput>
+      <IonButton
+        data-testid="submit-button"
+        onClick={() => {
+          setPlayerList([...playerList, playerName]);
+          setPlayerName("");
+        }}
+      >
+        submit
+      </IonButton>
+      <IonList data-testid="player-list">
+        <IonReorderGroup
+          disabled={false}
+          data-testid="reorder-button"
+          onIonItemReorder={handleReorder}
+        >
+          {playerList.map((player, index) => {
+            return (
+              <IonReorder>
+                <IonItem key={index}>
+                  <IonLabel>{player}</IonLabel>
+                  <IonReorder slot="end"></IonReorder>
+                </IonItem>
+              </IonReorder>
+            );
+          })}
+        </IonReorderGroup>
+      </IonList>
     </div>
   );
 };
